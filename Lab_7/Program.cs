8b54502b14d467b368975c145ddf9ebb9cad546c
Twinker25 <shashokd@gmail.com>
2023-04-13 19:35:27 +0300

    Add Task1

--- Lab_7/Program.cs
+++ Lab_7/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Enter size of array: ");
+            int size = int.Parse(Console.ReadLine());  
+            Array array = new Array(size);
+            array.Show();
+            Console.Write("\n\nEnter information message: ");
+            string info = Console.ReadLine();
+            array.Show(info);
         }
     }
 }
---
+++ Lab_7/Task1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_7
+{
+    interface IOutput
+    {
+        void Show();
+        void Show(string info);
+    }
+
+    class Array : IOutput
+    {
+        public int size;
+        public Array(int size)
+        {
+            this.size = size;
+        }
+
+        public virtual void Show()
+        {
+            int[] arr = new int[size];
+            Random rnd = new Random();
+            Console.Write("Array: ");
+            for (int i = 0; i < size; i++) 
+            {
+                arr[i] = rnd.Next(1, 10);
+                Console.Write(arr[i] + " ");
+            }
+        }
+        public virtual void Show(string info)
+        {
+            Show();
+            Console.WriteLine($"\nInformation message: {info}");
+        }
+    }
+}
